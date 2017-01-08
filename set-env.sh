796b79a0e5ca2655b8e0f7ef9a3a4564a2519c59
Andreas Streichardt <andreas.streichardt@gmail.com>
2017-01-08 10:22:28 +0100

    setenv

---
+++ set-env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+export PATH=$PATH:$HOME/Library/Android/sdk/tools/:$HOME/Library/Android/sdk/build-tools/25.0.2/:$HOME/Library/Android/sdk/platform-tools/:$HOME/Library/Android/android-ndk-r13b/
